6987e4fc511551a7f8e17f1b3c081e4f098cdce1
Florian Gardy <florian.gardy@gmail.com>
2019-03-21 16:44:18 +0100

    Added 5 travels

--- src/App.js
+++ src/App.js
@@ -11,16 +11,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        <Travel
-          destination="New York"
-          country="U.S.A"
-          photo="https://cdn.pixabay.com/photo/2014/05/02/23/46/new-york-city-336475_960_720.jpg"
-          distance="3000 km" />
-        <Travel
-          destination="Bayonne"
-          country="France"
-          photo="https://cdn.pixabay.com/photo/2018/11/11/13/23/bayonne-3808751_960_720.jpg"
-          distance="15 km" />
+        <Travel />
       </div>
     );
   }
--- src/Travel.js
+++ src/Travel.js
@@ -1,14 +1,54 @@
 // src/Travel.js
 import React from "react";
 
+const travels = [
+    {
+        "id": "1",
+        "destination": "Paris",
+        "country": "France",
+        "distance": "500 km",
+        "photo": "http://levieuxbougon.l.e.pic.centerblog.net/o/3af92c39.jpg"
+    },
+    {
+        "id": "2",
+        "destination": "Tokyo",
+        "country": "Japan",
+        "distance": "2500 km",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQA5cI9Rr8EeBNjJ0LGlUKO_CzLUFJxGDyHOaHOcm322uKp7g32"
+    },
+    {
+        "id": "3",
+        "destination": "Sydney",
+        "country": "Australia",
+        "distance": "3200 km",
+        "photo": "http://www.bourlingueurs.com/australie/fotos/fotos_sydney/opera_29.jpg"
+    },
+    {
+        "id": "4",
+        "destination": "Moscow",
+        "country": "Russia",
+        "distance": "1200 km",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/%E5%8F%B0%E4%B8%AD%E4%B8%83%E6%9C%9F_%28cropped%29.jpg/700px-%E5%8F%B0%E4%B8%AD%E4%B8%83%E6%9C%9F_%28cropped%29.jpg"
+    },
+    {
+        "id": "5",
+        "destination": "New York",
+        "country": "U.S.A",
+        "distance": "2100 km",
+        "photo": "http://www.bluetigercoffee.com/wp-content/uploads/2016/05/NewYorkContactPageBanner.jpg"
+    }
+]
+
 const Travel = (props) => (
     <div class="travel">
-        <ul>
-            <li>Destination: {props.destination}</li>
-            <li>Country: {props.country}</li>
-            <li>Distance: {props.distance}</li>
-        </ul>
-        <img src={props.photo} alt="Travel destination"/>
+        <p>
+            {travels.map(travel => (
+                <div>
+                    <p>Destination: {travel.destination}, country: {travel.country}, distance: {travel.distance}</p>
+                    <img src={travel.photo} alt="building" />
+                </div>
+            ))}
+        </p>
         <hr />
     </div>
 );
